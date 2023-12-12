bf2dc8ead2b0cb64fbe6a66a138a2dafd3c1cbc8
yyjc <yyjc2021@gmail.com>
2023-12-12 12:39:56 +0800

    test

---
+++ src/main/java/com/example/filedemo/demos/web/BasicController.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.filedemo.demos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+@Controller
+public class BasicController {
+
+    // http://127.0.0.1:8080/hello?name=lisi
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name,String age) {
+        return "Hello " + name+":age："+age;
+    }
+
+    @RequestMapping("/hello2")
+    @ResponseBody
+    public String hello2(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
+        return "Hello2 " + name;
+    }
+
+    // http://127.0.0.1:8080/user
+    @RequestMapping("/user")
+    @ResponseBody
+    public User user() {
+        User user = new User();
+        user.setName("theonefx");
+        user.setAge(666);
+        return user;
+    }
+
+    // http://127.0.0.1:8080/save_user?name=newName&age=11
+    @RequestMapping("/save_user")
+    @ResponseBody
+    public String saveUser(User u) {
+        return "user will save: name=" + u.getName() + ", age=" + u.getAge();
+    }
+
+    // http://127.0.0.1:8080/html
+    @RequestMapping("/html")
+    public String html() {
+        return "index.html";
+    }
+
+    @ModelAttribute
+    public void parseUser(@RequestParam(name = "name", defaultValue = "unknown user") String name
+            , @RequestParam(name = "age", defaultValue = "12") Integer age, User user) {
+        user.setName("zhangsan");
+        user.setAge(18);
+    }
+
+    @RequestMapping("/file")
+    @ResponseBody
+    public String file(){
+        // 指定要读取的文件路径
+        //String filePath = "data/app/apiclient_key.pem";
+        String filePath = "/data/app/apiclient_key.pem";
+        try {
+            System.out.println("路径"+filePath);
+            FileInputStream inputStream = new FileInputStream(filePath);
+            Throwable var2 = null;
+
+            String var3;
+            try {
+                var3 = new String(toByteArray(inputStream), StandardCharsets.UTF_8);
+            } catch (Throwable var13) {
+                var2 = var13;
+                throw var13;
+            } finally {
+                if (inputStream != null) {
+                    if (var2 != null) {
+                        try {
+                            inputStream.close();
+                        } catch (Throwable var12) {
+                            var2.addSuppressed(var12);
+                        }
+                    } else {
+                        inputStream.close();
+                    }
+                }
+
+            }
+
+            return var3;
+        } catch (IOException var15) {
+            throw new UncheckedIOException(var15);
+        }
+
+    }
+
+    public byte[] toByteArray(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        byte[] data = new byte[8192];
+
+        int nRead;
+        while((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        return buffer.toByteArray();
+    }
+}
